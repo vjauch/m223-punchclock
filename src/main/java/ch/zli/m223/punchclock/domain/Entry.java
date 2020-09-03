125a447431135f90f1cb928a54f0ad9f1e233274
Valentin Jauch <valentin.jauch@bluewin.ch>
2020-09-03 05:13:38 -0700

    Add category field to entry

--- src/main/java/ch/zli/m223/punchclock/domain/Entry.java
+++ src/main/java/ch/zli/m223/punchclock/domain/Entry.java
@@ -26,6 +26,10 @@ public class Entry {
     @Column(nullable = false)
     private LocalDateTime checkOut;
 
+    @ManyToOne()
+    @JoinColumn(nullable = false)
+    private Category category;
+
     public Long getId() {
         return id;
     }
@@ -48,6 +52,14 @@ public class Entry {
 
     public void setCheckOut(LocalDateTime checkOut) {
         this.checkOut = checkOut;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     @AssertTrue(message = "Invalid date range. Checkin must not be after checkout!")
